5437089c0a947d05766908bbce6da1d853629f99
Rajpreet Kaur <kaurrajpreet2018@gmail.com>
2023-10-19 23:34:25 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import {Route , Routes} from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>}></Route>
+      </Routes> 
     </div>
   );
 }
