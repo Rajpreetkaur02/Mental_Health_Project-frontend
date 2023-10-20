a6ef5a59c0a6689e562c40ee7ebe4256676ee377
Rajpreet Kaur <kaurrajpreet2018@gmail.com>
2023-10-20 18:16:55 +0530

    added hero section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 const HomePage = () => {
   return (
     <div>
-      hello
+      <HeroSection/>
     </div>
   )
 }
