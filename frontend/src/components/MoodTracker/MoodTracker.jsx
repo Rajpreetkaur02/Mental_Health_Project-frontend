b349bcea5aeada0f0365117d96f314c35f0d03c2
kbagga19 <baggakaran19@gmail.com>
2023-11-20 14:48:03 +0530

    linked mood tracker to database

--- frontend/src/components/MoodTracker/MoodTracker.jsx
+++ frontend/src/components/MoodTracker/MoodTracker.jsx
@@ -1,42 +1,69 @@
-import React, { useState } from 'react';
-import './MoodTracker.css';
+import React, { useEffect, useState } from 'react'; 
+import './MoodTracker.css'; 
 
-const MoodTracker = () => {
-  const [mood, setMood] = useState('😐 Neutral');
-  const [moodHistory, setMoodHistory] = useState([]);
+const MoodTracker = () => { 
+  const [mood, setMood] = useState('😐 Neutral'); 
+  const [moodHistory, setMoodHistory] = useState([]); 
+  
+  useEffect(() => { 
+    fetch(`http://localhost:8080/extra/getMood/${localStorage.getItem('id')}`,{ 
+      crossDomain: true, 
+      headers: { 'Content-Type':'application/json', 
+        Accept: "application/json", 
+        "Access-Control-Allow-Origin": "*", 
+        'Authorization': `Bearer ${localStorage.getItem('token')}` 
+      } 
+    }).then(res => res.json()) 
+    .then((data) => { 
+      console.log(data) 
+      setMoodHistory(data) 
+    }); 
+  }) 
+  
+  const handleMoodChange = newMood => { 
+    setMood(newMood); 
+    console.log(mood) 
+    console.log(moodHistory) 
 
-  const handleMoodChange = newMood => {
-    setMood(newMood);
-    setMoodHistory([...moodHistory, { mood: newMood, timestamp: new Date().toLocaleString()}]);
+    fetch(`http://localhost:8080/extra/addMood/${localStorage.getItem('id')}`,{ 
+      method: 'PUT', 
+      headers: { 
+        'Content-Type':'application/json', 
+        Accept: "application/json", 
+        "Access-Control-Allow-Origin": "*", 
+        'Authorization': `Bearer ${localStorage.getItem('token')}` 
+      }, 
+      body: JSON.stringify({"mood": newMood, "timestamp": new Date().toLocaleString()}), 
+    }); 
   };
-
-  return (
+  
+  return ( 
     <div className="mood-tracker">
-      <h3>How are you feeling?</h3>
-      <div className="mood-buttons">
-        <button onClick={() => handleMoodChange('😊 Happy')}>😊 Happy</button>
-        <button onClick={() => handleMoodChange('😐 Neutral')}>😐 Neutral</button>
-        <button onClick={() => handleMoodChange('😞 Sad')}>😞 Sad</button>
-      </div>
-      <div className="mood-history">
-        <h3>Mood History</h3>
-        {moodHistory.length === 0 ? (
-          <ul className='listMoodHistory'>
-            <li>No Record!</li>
-          </ul>
-        ) : (
-          <ul className='listMoodHistory'>
-            {moodHistory.map((entry, index) => (
-              <li key={index}>
-                <strong>{entry.mood}</strong><br/> at {entry.timestamp}
-                <hr style={{marginTop: '10px'}}/>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+    <h3>How are you feeling?</h3>
+    <div className="mood-buttons">
+      <button onClick={() => handleMoodChange('😊 Happy')}>😊 Happy</button>
+      <button onClick={() => handleMoodChange('😐 Neutral')}>😐 Neutral</button>
+      <button onClick={() => handleMoodChange('😞 Sad')}>😞 Sad</button>
     </div>
+    <div className="mood-history">
+      <h3>Mood History</h3>
+      {moodHistory.length === 0 ? (
+        <ul className='listMoodHistory'>
+          <li>No Record!</li>
+        </ul>
+      ) : (
+        <ul className='listMoodHistory'>
+          {moodHistory.map((entry, index) => (
+            <li key={index}>
+              <strong>{entry.mood}</strong><br/> at {entry.timestamp}
+              <hr style={{marginTop: '10px'}}/>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  </div>
   );
-};
+}
 
 export default MoodTracker;
