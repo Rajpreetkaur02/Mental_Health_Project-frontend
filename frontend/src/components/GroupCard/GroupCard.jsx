830180bbf08148de82c91eb3dcb634536f5fc174
Rajpreet Kaur <kaurrajpreet2018@gmail.com>
2023-11-20 12:16:37 +0530

    updated community page

--- frontend/src/components/GroupCard/GroupCard.jsx
+++ frontend/src/components/GroupCard/GroupCard.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import '../../styles/Community.css'
+import './GroupCard.css'
 import { Link } from 'react-router-dom'
+import { IoLocationOutline } from "react-icons/io5";
+import { GrGroup } from "react-icons/gr";
+import { MdOutlinePublic } from "react-icons/md";
 
 const GroupCard = (data) => {
   return (
     <div>
       <div className="groupcardcontainer">
         <div className="groupcardtitle">{data.title}</div>
-        <div className="groupcardmembers">{data.members} members</div>
-        <div className="groupcardorganizer">Organized by {data.organizer}</div>
-        <div className="groupcarddesc">{data.about}</div>
+        <div className="groupcardsubtitle">
+          <div className="cardbox"><span><IoLocationOutline/>{data.location}</span></div>
+          <div className="cardbox"><span><MdOutlinePublic/>public</span></div>
+          <div className="cardbox"><span><GrGroup/><span id='grpmembers'>{data.members}</span></span></div>
+        </div>
+        {/* <div className="groupcardmembers"><MdOutlineGroupAdd size={20}/>{data.members} members</div> */}
+        <div className="groupcardorganizer">Organized by <span id='grporganizer'>{data.organizer}</span></div>
+        <div className="groupcardtopics">{data.topics}</div>
+        {/* <div className="groupcarddesc" dangerouslySetInnerHTML={{__html: data.about.slice(0,20)}}></div> */}
         <Link className="groupcardbutton" to={`/groupdesc/${data._id}`}>
-          <button>find out more</button>
+          <button>Find out more</button>
         </Link>
       </div>
     </div>
