8da28f36e6eb400975d4fee1ed03a2d8c8aa961f
kbagga19 <baggakaran19@gmail.com>
2023-11-24 21:57:03 +0530

    integrated chatbot

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
 
                     </div>
                 </div>
-                <Chatbot />
+                {/* <Chatbot /> */}
             </div>
         </div>
     )
