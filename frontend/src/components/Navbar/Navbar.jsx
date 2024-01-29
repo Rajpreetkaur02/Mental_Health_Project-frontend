efd9aa33844aa5b78ffedfa5e7f9d7498dcb54dc
kbagga19 <baggakaran19@gmail.com>
2024-01-29 13:59:54 +0530

    responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -129,7 +129,7 @@ const Navbar = ({ isHomePage }) => {
               <FaRegCircleUser size={20} />
             </li>
             <li className={`${isHomePage ? "homepage-greeting" : "greeting"}`}>
-              Hello, {localStorage.getItem("name")}
+              Hello,<br/> {localStorage.getItem("name")}
             </li>
           </ul>
           <div
